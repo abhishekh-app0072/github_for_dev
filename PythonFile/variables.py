25c2ca1001f36d9eae079fa19ee89a4992de3f1b
abhishekh-app0072 <abhishekprasad0072@gmail.com>
2026-01-23 10:34:44 +0000

    add new

--- PythonFile/variables.py
+++ PythonFile/variables.py
@@ -1,7 +1,21 @@
-s = "apple"
-print(s)
+# s = "apple"
+# print(s)
 
-s= " Abhishek Prasad"
-age = 25 
-city = "New York"
-print(s,age,city)
+# s= " Abhishek Prasad"
+# age = 25 
+# city = "New York"
+# print(s,age,city)
+
+x = 5
+name = "honey"
+print(x)
+print(name)
+
+# Below listed variable names are valid:
+age = 23
+_colour = "red-pink"
+total_score = "30"
+ #  Below listed variables names are invalid:
+'''1name = "Error"  # Starts with a digit
+class = 10       # 'class' is a reserved keyword
+ user-name = "Doe"  # Contains a hyphen '''
