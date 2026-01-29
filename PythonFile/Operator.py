7606f2535f2f2db305c6c3270ba59d6d956521b8
abhishekh-app0072 <abhishekprasad0072@gmail.com>
2026-01-29 12:56:41 +0000

    add this

---
+++ PythonFile/Operator.py
@@ -0,0 +1,23 @@
+''' Python  Operator'''
+
+''' In Python programming, Operators in general are used to perform operations on values and variables.
+
+- Operators: Special symbols like -, + , * , /, etc.
+- Operands: Value on which the operator is applied.
+'''
+
+
+# Types of Operators in Python
+
+''' Arithmetic Operators'''
+
+a  = 10
+b  = 15 
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
