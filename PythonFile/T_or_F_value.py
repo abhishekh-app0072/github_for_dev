9da8cdab97d520b15864e4fcebf775da090c6912
abhishekh-app0072 <abhishekprasad0072@gmail.com>
2026-01-29 12:34:54 +0000

    add in day 3

---
+++ PythonFile/T_or_F_value.py
@@ -0,0 +1,18 @@
+# Built-in bool() function
+print(bool(7))      
+print(bool(0))      
+print(bool([1,2,3]))
+print(bool([]))      
+print(bool(None))
+
+
+n1 = 7
+n2 = 4
+
+if  n1 % 2 == 0: # replace the value n1 --> n2 or might be n1 
+    print("Even")
+else :
+    print("odd")
+
+  
+
