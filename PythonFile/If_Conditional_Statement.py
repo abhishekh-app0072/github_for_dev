6a39c40eae6681fe885bcda9dea05886e5058aba
abhishekh-app0072 <abhishekprasad0072@gmail.com>
2026-02-04 07:23:42 +0000

    day 06 in python

---
+++ PythonFile/If_Conditional_Statement.py
@@ -0,0 +1,51 @@
+# Conditional Statements in Python
+
+  # If Conditional Statement
+'''
+age = 20 
+if  age>= 18:
+    print(" Voted")
+'''
+
+
+
+
+'''
+age =  12
+if age >= 10: print("free bus tickets")
+
+'''
+
+
+
+'''
+n = 19
+if n >= 18:
+    print("YES, you are eligble for Vote")
+else:
+    print("your are not eligble for vote ") 
+'''
+
+
+'''
+age = int(input("enter your age :"))
+
+if age <= 12:
+    print("free pass")
+else:
+    print("paid pass")
+'''
+
+
+marks = int(input("enter your marks:"))
+res = "pass" if marks >= 40 else "fail"
+print(f"Result :{res}")
+
+'''marks = 45
+res = "Pass" if marks >= 40 else "Fail"
+print(f"Result: {res}")'''
+
+
+# elif Statement
+
+
