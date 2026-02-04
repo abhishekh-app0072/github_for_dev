5363857b3201d383c402d9303e0bf56372ccd297
abhishekh-app0072 <abhishekprasad0072@gmail.com>
2026-02-04 12:57:34 +0000

    add day 06  in python

--- PythonFile/problem_solve/Problem_1.py
+++ PythonFile/problem_solve/Problem_1.py
@@ -100,6 +100,7 @@ else :
 
   # Problem 1: Login with username and password
 
+'''
 user =(input("enter your user  name: "))
 psw = (input("Enter Your pass word  : "))
 
@@ -109,6 +110,95 @@ if user == "abhishek" :
     else: 
         print("wrong password pls check, again enter your correct username or password ")
 
- 
+ '''
+
 # Problem 2: Check exam pass and scholarship eligibility  
 
+'''
+marks = int(input("enter your marks : "))
+
+if marks >= 50 :
+    if marks >= 80:
+        print("eligible for scholarship")
+    else:
+        print("Not eligible for sch")
+else:
+    print("failed")
+    '''
+
+
+ # Ternary Statement
+
+  # Problem 1: Check if number is even
+'''
+num = int(input("enter your num: "))
+if num % 2  == 0 : print("even") 
+else : print("odd")
+
+# num = int(input("Enter a number: "))
+# print("Even" if num % 2 == 0 else "Odd")
+
+'''
+
+# Problem 2: Compare two numbers
+
+'''
+ int(input("enter your number " ))
+b = int(input("enter your number " ))
+
+print( "a is greater than b "  if a>b else " b is greater than a")
+'''
+
+# Problem 3: Temperature check
+'''
+temp =  float(input("enter your temp value "))
+print("hot" if temp >= 30 else "cool")
+'''
+
+# Match- case Statement 
+
+#Problem 1: Assign grade
+
+# grade = input("Enter your grade (A/B/C): ").upper()
+
+# match grade:
+#     case "A":
+#         print("Excellent")
+#     case "B":
+#         print("Good")
+#     case "C":
+#         print("Average")
+#     case _:
+#         print("Fail")
+
+#Problem 2: Activity Suggestion based on weather condition
+
+# weather = input("Enter the weather (sunny/rainy/cloudy/snowy): ").lower()
+
+# match weather:
+#     case "sunny":
+#         print("Great day for a picnic!")
+#     case "rainy":
+#         print("Stay indoors and read a book.")
+#     case "cloudy":
+#         print("Perfect time for a walk.")
+#     case "snowy":
+#         print("Build a snowman or go skiing!")
+#     case _:
+#         print("Unknown weather condition.")
+
+
+# Problem 3: Mobile notification settings based on user profile mode
+mode = input("Enter phone mode (silent/vibrate/loud/do not disturb): ").lower()
+
+match mode:
+    case "silent":
+        print("Notifications are muted.")
+    case "vibrate":
+        print("Phone will vibrate for notifications.")
+    case "loud":
+        print("All notifications will play sound.")
+    case "do not disturb":
+        print("No calls or notifications will come through.")
+    case _:
+        print("Invalid mode selected.")
